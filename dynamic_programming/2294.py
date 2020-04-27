e33fa43d4d027a848354dd5cc5e74f1f91b8a418
최덕경 <hjcdg197@gmail.com>
2020-04-27 21:39:28 +0900

    18

---
+++ dynamic_programming/2294.py
@@ -0,0 +1,32 @@
+from sys import stdin
+
+
+N, K = tuple(map(int, stdin.readline().split()))
+C = [None] + [int(stdin.readline()) for _ in range(N)]
+
+# D[i][j] : i번째 동전까지 사용하여 j원을 만드는 동전의 최소 개수
+D = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+
+for j in range(1, K + 1):
+	D[0][j] = -1
+
+for i in range(1, N + 1):
+	for j in range(1, K + 1):
+		if j >= C[i]:
+			c1, c2 = D[i][j - C[i]], D[i - 1][j]
+			if c1 == -1 and c2 == -1:
+				D[i][j] = -1
+			elif c1 == -1:
+				D[i][j] = c2
+			elif c2 == -1:
+				D[i][j] = c1 + 1
+			else:
+				D[i][j] = min(c1 + 1, c2)
+		else:
+			D[i][j] = D[i - 1][j]
+
+print(D[N][K])
+
+"""
+<또 다른 풀이>
+"""
