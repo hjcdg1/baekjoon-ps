23a94b11611a684edb6344077a09841514b4a4e5
최덕경 <hjcdg197@gmail.com>
2021-09-06 18:34:07 +0900

    45

--- dynamic_programming/10942.py
+++ dynamic_programming/10942.py
@@ -2,23 +2,24 @@ from sys import stdin
 
 
 N = int(stdin.readline())
-A = [-1] + list(map(int, stdin.readline().split()))
+A = [None] + list(map(int, stdin.readline().split()))
 M = int(stdin.readline())
-Q = [tuple(map(int, stdin.readline().split())) for _ in range(M)]
+Q = [list(map(int, stdin.readline().split())) for _ in range(M)]
 
 # D[i][j] : A[i] ~ A[j]가 팰린드롬인지 여부
-D = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+D = [None] + [[None] + [0 for _ in range(N)] for _ in range(N)]
 
 for i in range(1, N + 1):
 	D[i][i] = 1
 
-for d in range(1, N):
+for i in range(1, N):
+	D[i][i + 1] = 1 if A[i] == A[i + 1] else 0
+
+for d in range(2, N):
 	for i in range(1, N - d + 1):
-		s, e = i, i + d
-		if s + 1 == e:
-			D[s][e] = 1 if A[s] == A[e] else 0
-		else:
-			D[s][e] = 1 if A[s] == A[e] and D[s + 1][e - 1] == 1 else 0
+		start = i
+		end = i + d
+		D[start][end] = D[start + 1][end - 1] if A[start] == A[end] else 0
 
 for q in Q:
 	print(D[q[0]][q[1]])
--- dynamic_programming/11049.py
+++ dynamic_programming/11049.py
@@ -2,21 +2,19 @@ from sys import stdin
 
 
 N = int(stdin.readline())
-M = [None] + [tuple(map(int, stdin.readline().split())) for _ in range(N)]
+M = [None] + [list(map(int, stdin.readline().split())) for _ in range(N)]
 
 # D[i][j] : M[i] ~ M[j]를 곱하는 최소 횟수
-D = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
-
-for i in range(1, N + 1):
-	D[i][i] = 0
+D = [None] + [[None] + [0 for _ in range(N)] for _ in range(N)]
 
 for d in range(1, N):
 	for i in range(1, N - d + 1):
-		s, e = i, i + d
+		start = i
+		end = i + d
 
 		min_D = float('inf')
-		for k in range(s, e):
-			min_D = min(min_D, D[s][k] + D[k + 1][e] + M[s][0] * M[k][1] * M[e][1])
-		D[s][e] = min_D
+		for k in range(start, end):
+			min_D = min(min_D, D[start][k] + D[k + 1][end] + M[start][0] * M[k][1] * M[end][1])
+		D[start][end] = min_D
 
 print(D[1][N])
