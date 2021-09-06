57b35d9bd143c39c4fbfa609f330b6582a6315c0
최덕경 <hjcdg197@gmail.com>
2021-09-07 00:53:02 +0900

    47

--- dynamic_programming/5557.py
+++ dynamic_programming/5557.py
@@ -2,7 +2,7 @@ from sys import stdin
 
 
 N = int(stdin.readline())
-A = [-1] + list(map(int, stdin.readline().split()))
+A = [0] + list(map(int, stdin.readline().split()))
 
 # D[i][j] : A[i]까지의 결과가 j인 경우의 수
 D = [[0 for _ in range(21)] for _ in range(N)]
@@ -11,13 +11,9 @@ D[1][A[1]] = 1
 
 for i in range(2, N):
 	for j in range(21):
-		if A[i] == 0:
-			D[i][j] = 2 * D[i - 1][j]
-		else:
-			D[i][j] = 0
-			if 0 <= j - A[i] <= 20:  # + A[i]
-				D[i][j] += D[i - 1][j - A[i]]
-			if 0 <= j + A[i] <= 20:  # - A[i]
-				D[i][j] += D[i - 1][j + A[i]]
+		if 0 <= j - A[i] <= 20:
+			D[i][j] += D[i - 1][j - A[i]]
+		if 0 <= j + A[i] <= 20:
+			D[i][j] += D[i - 1][j + A[i]]
 
-print(D[N - 1][A[-1]])
+print(D[N - 1][A[N]])
